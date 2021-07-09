f38a125278c5fe54ac244c662a6466348226ffa0
Phu Pham <phamhungphu97@gmail.com>
2021-07-09 15:16:06 +0100

    Implemented charts for single accounts

--- src/components/transactions/SingleAccount.js
+++ src/components/transactions/SingleAccount.js
@@ -1,23 +1,72 @@
 import React from "react"
+import { Line } from "react-chartjs-2"
 
 const SingleAccounts = ({ transactions, balances }) => {
     if(transactions[0]){
+        transactions = transactions.map(transaction => {
+            const date = new Date(transaction.details.completed)
+            const newDate =  date.getFullYear()+"-" + (date.getMonth()+1) + "-"+date.getDate()
+            return { ...transaction, details: { ...transaction.details, completed: newDate } }
+        })
+        const dates = transactions.map(transaction => transaction.details.completed)
+        let startingDate = null
+        const balanceList = transactions.map(transaction => transaction.details.new_balance.amount)
+        const data ={
+            labels : dates,
+            datasets: [{
+                label: "Balance (EUR)",
+                data: balanceList,
+                fill: false,
+                lineTension: 0.5,
+                backgroundColor: "rgb(255, 99, 132)",
+                borderColor: "rgba(255, 99, 132, 0.2)",
+            }]
+        }
+        const options = {
+            scales: {
+                yAxes: {
+                    grid: { display: true, },
+                    ticks: { beginAtZero: true } },
+                xAxes: {
+                    grid: { display: false, },
+                    ticks: {
+                        display: true,
+                        autoSkip: true,
+                        autoSkipPadding: 50,
+                    }
+                } },
+        }
         return(
-            <ul>
-                {transactions.map(transaction =>
-                    <li key = {transaction.id}>
+            <div>
+                <div style ={{ height: "500px", width: "1000px" }} >
+                    <Line data={data} options={options} />
+                </div>
+                <ul>
+                    {transactions.map((transaction) => {
+                        if(transaction.details.completed !== startingDate){
+                            startingDate = transaction.details.completed
+                        }else{
+                            transaction.details.completed = null
+                        }
+                        return(
+                            <div key = {transaction.id}>
+                                {transaction.details.completed ? <h2>{transaction.details.completed}</h2> : console.log()}
+                                <li >
                             Balance: {transaction.details.new_balance.amount} Transfer amount: {transaction.details.value.amount} Description: {transaction.details.description}
-                    </li>)}
-            </ul>
+                                </li>
+                            </div>
+                        )})}
+                </ul>
+            </div>
         )
     }else if(balances[0]){
         return (
-            <ul>
+            <div>
                 {balances.map(balance =>
-                    <li key = {balance.amount}>
+                    <h3 key = {balance.amount}>
                             Balance: {balance.amount}
-                    </li>)}
-            </ul>
+                    </h3>)}
+            </div>
         )
     }
     return(
